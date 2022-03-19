c985754b34dbacdc01323c5a124c20d8cdbbdb02
ytte <1026190684@qq.com>
2022-03-19 20:30:53 +0800

    20220319203052

--- zac.bat
+++ zac.bat
@@ -2,6 +2,4 @@ git add .
 start /min /w mshta vbscript:setTimeout("window.close()",2200)
 git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
 start /min /w mshta vbscript:setTimeout("window.close()",2200)
-git push 
-start /min /w mshta vbscript:setTimeout("window.close()",5200)
 exist
---
+++ zp.bat
@@ -0,0 +1,3 @@
+git push 
+start /min /w mshta vbscript:setTimeout("window.close()",5200)
+exist
